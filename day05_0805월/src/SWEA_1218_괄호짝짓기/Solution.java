5510c51cf2fca3d1e194bfaf299e1e2c80b2122e
pasongvan <pasongvaan@gmail.com>
2024-08-05 21:48:16 +0900

    괄호짝짓기

--- day05_0805월/src/SWEA_1218_괄호짝짓기/Solution.java
+++ day05_0805월/src/SWEA_1218_괄호짝짓기/Solution.java
@@ -1,34 +1,74 @@
 package SWEA_1218_괄호짝짓기;
 
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Solution {
+	
+	// 스택 선언
+	static List<String> openBracket;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		for (int test_case=1; test_case<=10; test_case++) {
+			// 변수 선언========================================
+			openBracket = new ArrayList<>();
+			int valid = 1; // 유효성 여부
+			int len = sc.nextInt();
+			String oneLine = sc.next();
 			
-			// 변수 설정 및 입력========================================
-			int len = sc.nextInt(); // 테스트 케이스 길이
-			String oneLine = sc.next(); // 한 줄 입력
-			int result = 1; // 유효성 여부 변수
-			int[] stack = new int[len]; // 괄호 스택 (소괄호 1, 중괄호 2, 대괄호 3, 꺽쇠괄호 4)
-			int top = -1; // 스택 top 변수
-			
-			// 괄호 열고 닫기==========================================
+			// 괄호 검사========================================
 			for (int i=0; i<len; i++) {
-				String letter = oneLine.substring(i, i+1); // 한글자씩
-				// 여는 괄호일 때
-				if (letter.equals("(") || letter.equals("{") || letter.equals("[") || letter.equals("<")) {
-					stack[++top] = letter; // push
+				String blacket = oneLine.substring(i, i+1);
+				
+				// 1. 여는 괄호인 경우
+				if (blacket.equals("(") || blacket.equals("{") || blacket.equals("[") || blacket.equals("<"))
+					push(blacket);
+				
+				// 2. 닫는 괄호인 경우
+				// 스택이 비어있으면 유효x
+				else if (isEmpty()) {
+					valid = 0;
+					break;
 				}
-				// 닫는 괄호일 때
 				else {
-					if (top==-1)
-						break; // 괄호 열지 않았는데 닫으려고 하면 중단
-					else if ()
+					// 닫는 괄호와 스택 top 종류가 다르면 유효x
+					String top = pop();
+					if ((top.equals("(") && !blacket.equals(")")) || (top.equals("{") && !blacket.equals("}")) || (top.equals("[") && !blacket.equals("]")) || (top.equals("<") && !blacket.equals(">"))) {
+						valid = 0;
+						break;
+					}
 				}
 			}
+			// 괄호 검사가 끝났는데도 스택이 비어있지 않다면 유효x
+			if (!isEmpty())
+				valid = 0;
 			
+			// 출력============================================
+			System.out.println("#" + test_case + " " + valid);
 		}
 	}
+	// push method============================================
+	static void push(String blacket) {
+		openBracket.add(blacket);
+	}
+	
+	// pop method=============================================
+	static String pop() {
+		String popOut = openBracket.get(openBracket.size()-1);
+		openBracket.remove(openBracket.size()-1);
+		return popOut;
+	}
+	
+	// peek method============================================
+	static String peek() {
+		return openBracket.get(openBracket.size()-1);
+	}
+	
+	// isEmpty method=========================================
+	static boolean isEmpty() {
+		return openBracket.size()==0;
+	}
+	
 }
