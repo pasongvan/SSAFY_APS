a16145afa3f75d331ed219e74a9d9d21501dc40d
pasongvan <pasongvaan@gmail.com>
2024-08-07 14:45:36 +0900

    perfect shuffle with self-implemented queue

--- day07_0807수/src/SWEA_3499_퍼펙트셔플/Solution.java
+++ day07_0807수/src/SWEA_3499_퍼펙트셔플/Solution.java
@@ -1,50 +1,82 @@
 package SWEA_3499_퍼펙트셔플;
 
-import java.util.Queue;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Solution {
 	
+	// 큐 선언
+	static String[] queue;
+	static int front = -1;
+	static int rear = -1;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		
 		for (int test_case=1; test_case<=T; test_case++) {
 			
-			// 변수 및 입력
-			Queue<String> queue1 = new LinkedList<>();
-			Queue<String> queue2 = new LinkedList<>();
+			// 입력 및 초기화
 			int len = sc.nextInt();
+			front = -1;
+			rear = -1;
+			queue = new String[len];
 			
-			// 짝수, 홀수 일 때 나누는 지점 지정
-			int startQ2;
-			if (len%2 == 0)
-				startQ2 = len/2;
-			else
-				startQ2 = len/2 + 1;
-			
-			// 큐1에 앞 절반 담기
-			for (int i=0; i<startQ2; i++) {
-				String element = sc.next();
-				queue1.add(element);
-			}
-			
-			// 큐2에 뒤 절반 담기
-			for (int i=startQ2; i<len; i++) {
-				String element = sc.next();
-				queue2.add(element);
-			}
-			
-			// 번갈아가면서 출력
-			System.out.print("#" + test_case + " ");
 			for (int i=0; i<len; i++) {
-				if (i%2==0) 
-					System.out.print(queue1.poll() + " ");
-				else 
-					System.out.print(queue2.poll() + " ");
+				String word = sc.next();
+				offer(word);
+			}
+			
+			// 출력
+			System.out.print("#" + test_case + " ");
+			// 홀수인경우 1회 먼저 시작
+			if (len%2==1) {
+				System.out.print(poll() + " ");
+				for (int i=0; i<len/2; i++) {
+					offer(poll());
+				}
+			}
+			// 큐가 빌 때까지 반복
+			int iter = len/2-1;
+			while(!isEmpty()) {
+				System.out.print(poll() + " ");
+				for (int i=0; i<iter; i++) {
+					offer(poll());
+				}
+				System.out.print(poll() + " ");
+				for (int i=0; i<iter; i++) {
+					offer(poll());
+				}
+				iter--;
 			}
 			System.out.println();
 		}
+	}
+	
+	// offer
+	static void offer(String item) {
+		if (isFull()) 
+			return;
+		queue[++rear] = item;
+	}
+	
+	// poll
+	static String poll() {
+		if (isEmpty())
+			return null;
+		String pollOut = queue[0];
+		rear--;
+		for (int i=0; i<=rear; i++) {
+			queue[i] = queue[i+1];
+		}
+		return pollOut;
+	}
+	
+	// isEmpty
+	static boolean isEmpty() {
+		return rear == -1;
+	}
+	
+	// ifFull
+	static boolean isFull() {
+		return rear == queue.length -1;
 	}
 }
