d2e377cfa321f87541a1313d5de0521a6dc57db2
pasongvan <pasongvaan@gmail.com>
2024-08-13 11:24:14 +0900

    연결스택구현

--- day10_0813화/src/연결스택구현/Solution.java
+++ day10_0813화/src/연결스택구현/Solution.java
@@ -7,7 +7,20 @@ class Node {
 
 public class Solution {
 	public static void main(String[] args) {
+		LinkedStack stack = new LinkedStack();
 		
+		System.out.println(stack.isEmpty());
+		
+		stack.push("마이크 트라웃");
+		stack.push("무키 베츠");
+		stack.push("애런 저지");
+		
+		System.out.println(stack.size());
+		
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		
+		System.out.println(stack.peek());
 	}
 }
 
@@ -15,14 +28,50 @@ class LinkedStack {
 	Node head = new Node();
 	int size;
 	
-	// 삽입 => head 바로 뒤가 top
-	void add(String data) {
+	LinkedStack() {}
+	
+	// push => head 바로 뒤가 top
+	void push(String data) {
 		Node newNode = new Node();
 		newNode.data = data;
 		
 		newNode.link = head.link;
 		head.link = newNode;
+		
+		size++;
 	}
 	
+	// pop
+	String pop() {
+		if (isEmpty()) {
+			System.out.println("스택 비어있음");
+			return null;
+		}
+		
+		String popOut = head.link.data;
+		head.link = head.link.link; // 링크 변경
+		
+		size--;
+		return popOut;
+	}
 	
+	// peek
+	String peek() {
+		if (isEmpty()) {
+			System.out.println("스택 비어있음");
+			return null;
+		}
+		
+		return head.link.data;
+	}
+	
+	// isEmpty
+	boolean isEmpty() {
+		return size==0;
+	}
+	
+	// size
+	int size() {
+		return size;
+	}
 }
